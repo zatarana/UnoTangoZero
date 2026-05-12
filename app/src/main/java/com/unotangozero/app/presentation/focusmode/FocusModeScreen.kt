7580b950c1a695a8d91185d55dd492b761c79c90
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-12 16:54:44 -0300

    Fix Alignment import in focus mode screen

--- app/src/main/java/com/unotangozero/app/presentation/focusmode/FocusModeScreen.kt
+++ app/src/main/java/com/unotangozero/app/presentation/focusmode/FocusModeScreen.kt
@@ -1,6 +1,5 @@
 package com.unotangozero.app.presentation.focusmode
 
-import androidx.compose.foundation.layout.Alignment
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -37,6 +36,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
