2b6b0a0027103eb9dea0d0b7f4fab72b79599ae2
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-12 21:37:25 -0300

    Initialize reminder notification channel on startup

--- app/src/main/java/com/unotangozero/app/TangoApp.kt
+++ app/src/main/java/com/unotangozero/app/TangoApp.kt
@@ -1,6 +1,7 @@
 package com.unotangozero.app
 
 import android.app.Application
+import com.unotangozero.app.notifications.TaskReminderReceiver
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,6 +9,7 @@ import timber.log.Timber
 class TangoApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        TaskReminderReceiver.ensureChannel(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
