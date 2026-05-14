7d8b25d15f0ef7c6f165bfe20fb6a6bf3bcb55bd
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-14 14:25:23 -0300

    Make financial account type serializable

--- app/src/main/java/com/unotangozero/app/domain/enums/FinancialAccountType.kt
+++ app/src/main/java/com/unotangozero/app/domain/enums/FinancialAccountType.kt
@@ -1,5 +1,8 @@
 package com.unotangozero.app.domain.enums
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class FinancialAccountType(val displayName: String) {
     CHECKING("Conta corrente"),
     SAVINGS("Poupança"),
