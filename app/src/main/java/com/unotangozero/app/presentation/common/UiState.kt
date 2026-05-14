9f1e92e083e84eae1f1e005633dfea69d8804134
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-14 14:34:55 -0300

    Add shared UiState model

---
+++ app/src/main/java/com/unotangozero/app/presentation/common/UiState.kt
@@ -0,0 +1,7 @@
+package com.unotangozero.app.presentation.common
+
+sealed class UiState<out T> {
+    data object Loading : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val message: String) : UiState<Nothing>()
+}
