aed248f9529f6a98b8748681d377f554c35bdd6f
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-14 14:33:04 -0300

    Split note repository binding into NoteModule

---
+++ app/src/main/java/com/unotangozero/app/di/NoteModule.kt
@@ -0,0 +1,17 @@
+package com.unotangozero.app.di
+
+import com.unotangozero.app.data.repositories.NoteRepositoryImpl
+import com.unotangozero.app.domain.repositories.NoteRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NoteModule {
+    @Binds
+    @Singleton
+    abstract fun bindNoteRepository(impl: NoteRepositoryImpl): NoteRepository
+}
